12413adce1c422976b3abb21667552d0df475634
wolfceo <loopool@126.com>
2023-08-25 17:08:16 +0800

    up

--- fan.py
+++ fan.py
@@ -60,10 +60,10 @@ def get_fan_conf():
             f.write(response.content)
 
 def diy_conf(content):
-    pattern = r'{"key":"js豆豆".*'
+    pattern = r'{"key":"豆豆".*'
     replacement = r'{"key":"js豆瓣","name":"🅱豆瓣┃首页","type":3,"api":"./JS/lib/drpy2.min.js","ext":"./JS/js/drpy.js","searchable": 0,"quickSearch": 0,"filterable": 1},'
     content = re.sub(pattern, replacement, content)
-    pattern = r'{"key":"csp_Bili"(.)*\n{"key":"csp_Biliych"(.)*\n{"key":"MV_vod"(.)*\n'
+    pattern = r'{"key":"Bili"(.)*\n{"key":"Biliych"(.)*\n'
     replacement = ''
     content = re.sub(pattern, replacement, content)
     pattern = r'{"key":"csp_Nbys"(.|\n)*(?={"key":"cc")'
